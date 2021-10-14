03cf7f8729edfa5c13979a6a91e7022370881e41
luthfiazi <luthfiazizi11@gmail.com>
2021-10-14 15:42:17 +0700

    simplify func root url

--- main.go
+++ main.go
@@ -9,13 +9,41 @@ import (
 func main(){
 	router :=gin.Default()
 
-	//membuat root url
-	router.GET("/", func(c *gin.Context)  {
-		c.JSON(http.StatusOK, gin.H{
-			"name": "Luthfi Azizi",
-			"bio": "A story of coding",
-		})
-	})
+	//membuat root url yang disarankan
+	router.GET("/", rootHandler)  
 
-	router.Run()
+	router.GET("/artikel", rootArtikel)
+
+	//--root url yang kurang disarankan--
+
+	// router.GET("/", func(c *gin.Context)  {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"name": "Luthfi Azizi",
+	// 		"bio": "A story of coding",
+	// 	})
+	// })
+
+	// router.GET("/artikel", func(c *gin.Context)  {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"title": "Learning Golang",
+	// 		"content": "Learning Go language programming is fund and amazing",
+	// 	})
+	// })
+
+	router.Run(":9090")
 } 
+
+//memperbaiki root func 
+func rootHandler(c *gin.Context){
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Luthfi Azizi",
+		"bio": "A story of coding",
+	})
+}
+
+func rootArtikel(c *gin.Context){
+	c.JSON(http.StatusOK, gin.H{
+		"title": "Learning Golang",
+		"content": "Learning Go language programming is fund and amazing",
+	})
+}
