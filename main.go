231d11fdb94e057b18c524a84de7cc85266c1d6c
luthfiazi <luthfiazizi11@gmail.com>
2021-10-14 16:20:30 +0700

    create func get API query string

--- main.go
+++ main.go
@@ -14,6 +14,10 @@ func main(){
 
 	router.GET("/artikel", rootArtikel)
 
+	router.GET("/books/:id/:title/:author", bookHandler)
+
+	router.GET("/query", queryHandler)
+
 	//--root url yang kurang disarankan--
 
 	// router.GET("/", func(c *gin.Context)  {
@@ -33,7 +37,9 @@ func main(){
 	router.Run(":9090")
 } 
 
-//memperbaiki root func 
+//memperbaiki root func yang lebih disarankan
+
+//get API 
 func rootHandler(c *gin.Context){
 	c.JSON(http.StatusOK, gin.H{
 		"name": "Luthfi Azizi",
@@ -46,4 +52,25 @@ func rootArtikel(c *gin.Context){
 		"title": "Learning Golang",
 		"content": "Learning Go language programming is fund and amazing",
 	})
+}
+
+//get API by Id atau parameter variabel lebih dari 1
+func bookHandler(c *gin.Context){
+	id := c.Param("id")
+	title := c.Param("title")
+	author := c.Param("author")
+
+	c.JSON(http.StatusOK, gin.H{
+		"id": id,
+		"title": title,
+		"author": author,
+	})
+}
+
+//get API by query string variabel lebih dari 1
+func queryHandler(c *gin.Context){
+	title := c.Query("title")
+	description := c.Query("description")
+
+	c.JSON(http.StatusOK, gin.H{"title":title, "description":description})
 }
