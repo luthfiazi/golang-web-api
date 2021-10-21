bc78bbf74d535a21cc38b413d1b2c6e517fb42b3
luthfiazi <luthfiazizi11@gmail.com>
2021-10-21 15:59:36 +0700

    validation error messages

--- main.go
+++ main.go
@@ -90,11 +90,16 @@ func postBooksHandler(c *gin.Context){
 	err := c.ShouldBindJSON(&bookInput)
 	if err != nil {
 
+		errorMessages := []string{}
 		for _, e:= range err.(validator.ValidationErrors) {
-			errorMessage := fmt.Sprintf("Error on filed %s, condition: %s", e.Field(), e.ActualTag())
-			c.JSON(http.StatusBadRequest, errorMessage)
-			return
+			errorMessage := fmt.Sprintf("Error on field %s, condition: %s", e.Field(), e.ActualTag())
+			errorMessages = append(errorMessages, errorMessage)
 		}
+
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": errorMessages,
+		})
+		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{
